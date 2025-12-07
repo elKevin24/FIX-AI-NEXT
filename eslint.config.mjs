1eb30d231f2334d863e611c198fde9fcdfeae263
elKevin24 <36934396+elKevin24@users.noreply.github.com>
2025-12-06 22:00:44 -0600

    revert: Revert attempts to fix linting

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,8 +1,14 @@
-import nextPlugin from "eslint-config-next";
+import { dirname } from "path";
+import { fileURLToPath } from "url";
+import { FlatCompat } from "@eslint/eslintrc";
 
-const eslintConfig = [
-  ...nextPlugin,
-  // You can add more configurations here
-];
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+const compat = new FlatCompat({
+  baseDirectory: __dirname,
+});
+
+const eslintConfig = [...compat.extends("next/core-web-vitals")];
 
 export default eslintConfig;
